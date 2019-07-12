cb818c4049ee018d8f210c65e3e82202a30126c4
Sonia Jain <baps@bapss-MacBook-Pro-2.local>
2019-07-12 17:23:09 +0530

    lists

--- src/App.js
+++ src/App.js
@@ -2,30 +2,102 @@ import React, { Component } from "react";
 import "./App.css";
 import Users from "./components/Users/Users";
 import Parent from "./components/ParentToChild/Parent";
+import User from "./components/Users/User";
+import UniqueID from "react-html-id";
 
 class App extends Component {
-  state = {
-    title: "First Title"
-  };
+  constructor() {
+    super();
+    UniqueID.enableUniqueIds(this);
+    this.state = {
+      title: "First Title",
+      users: [
+        {
+          id: this.nextUniqueId(),
+          name: "ABCD1",
+          age: 10
+        },
+        {
+          id: this.nextUniqueId(),
+          name: "ABCD2",
+          age: 20
+        },
+        {
+          id: this.nextUniqueId(),
+          name: "ABCD3",
+          age: 30
+        },
+        {
+          id: this.nextUniqueId(),
+          name: "ABCD4",
+          age: 40
+        }
+      ]
+    };
+  }
 
-  changeTitle = (newTitle) => {
+  changeTitle = newTitle => {
     this.setState({
       title: newTitle
     });
   };
 
+  deleteUser = (index, e) => {
+    console.log(this.nextUniqueId());
+    //const users = [...this.state.users];
+    const users = Object.assign([], this.state.users);
+    users.splice(index, 1);
+    this.setState({ users: users });
+  };
+
+  changeUserName = (id, event) => {
+    if (event.target.value.length === 0) {
+      return;
+    }
+    const index = this.state.users.findIndex(user => {
+      return user.id === id;
+    });
+
+    const user = Object.assign({}, this.state.users[index]);
+    user.name = event.target.value;
+
+    const users = Object.assign([], this.state.users);
+    users[index] = user;
+
+    this.setState({ users: users });
+  };
+
   render() {
     return (
       <div className="App">
-        <div>
-          <Users />
-        </div>
+        <div>{/* <Users /> */}</div>
         <div>
           <Parent
             buttonClickEvent1={this.changeTitle.bind(this, "Second Title")}
             buttonClickEvent2={this.changeTitle.bind(this, "Third Title")}
             title={this.state.title}
           />
+        </div>
+        <div>
+          <ul>
+            {this.state.users.map((user, index) => {
+              return (
+                <div>
+                  <User
+                    delEvent={this.deleteUser.bind(this, index)}
+                    age={user.age}
+                    changeEvent={this.changeUserName.bind(this, user.id)}
+                    key={user.id}
+                  >
+                    {user.name}
+                  </User>
+                </div>
+              );
+              {
+                /*key is to avoid the warning*/
+              }
+            })}
+          </ul>
         </div>
       </div>
     );
--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,8 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 
-
-const User = (props) => {
-  return <div>User</div>;
+const User = props => {
+  let age = props.age >= 0 ? props.age : "NA";
+  if (props.children) {
+    return (
+      <li>
+        <span>
+          name: {props.children} | age: {age}
+        </span>
+        <input onChange={props.changeEvent} value={props.children} />
+        <button onClick={props.delEvent}>Delete</button>
+      </li>
+    );
+  } else {
+    return <li>Invalid Entry</li>;
+  }
 };
 
 export default User;
